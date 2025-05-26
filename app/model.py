2959e965ce0fb2d030255f096437f4923fb2779f
Victor Momodu <victoramomodu@gmail.com>
2025-05-26 12:45:41 +0100

    feat: add model

--- app/model.py
+++ app/model.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 data = pd.read_csv('./data/weather_classification_data.csv')
 
